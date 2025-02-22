40af859e2a58dea3a6890992a0dc666fb7a4a68c
Maria Clara Silva <109687213+mariclaradev@users.noreply.github.com>
2025-02-22 16:32:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+        setTimeout(() => {
+            document.getElementById("main-text").style.opacity = "1";
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById("sub-text").style.opacity = "1";
+            document.getElementById("icons").style.opacity = "1";
+        }, 2500);
