b9755ee2a4b6448e0c3dedc9415e9e243679f6a0
Maria Clara Silva <109687213+mariclaradev@users.noreply.github.com>
2025-02-22 18:02:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-         setTimeout(() => {
+<script>
+        // Faz o primeiro texto aparecer
+        setTimeout(() => {
             document.getElementById("main-text").style.opacity = "1";
         }, 500);
 
+        // Faz o segundo texto e os ícones aparecerem após o primeiro
         setTimeout(() => {
             document.getElementById("sub-text").style.opacity = "1";
             document.getElementById("icons").style.opacity = "1";
         }, 2500);
+    </script>
